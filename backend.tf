4b4c6c635b9d92b3af0d5fd4607fb3658bcf2204
unknown <Eng-mike>
2025-11-08 20:44:49 -0500

    new msg

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "week10-k-terraform"
+    bucket         = "mk-bucket-terraform"
     key            = "test/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
